894d35b5b330a4eabf5d5c3bf70329caeb3e09f4
Ravi <raviprasadrp01@gmail.com>
2022-12-20 23:35:26 +0530

    good

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express'); 
 var bodyParser = require('body-parser');
 
-const route = require('./routes/route.js');
+const route = require('./routes/route.js'); //import route content (address)
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser.json());   //converting machinery readable data to j.son format
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', route);
+app.use('/', route);             // channelise request towards route
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
